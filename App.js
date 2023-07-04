ca21cbfc7176ed12d592ff6063e63d9b7235206e
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2023-07-03 22:02:04 -0300

    :dart: Init

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import Home from "./src/telas/home";
+
+function App() {
+  return <SafeAreaView>
+    <Home/>
+  </SafeAreaView>
+}
+
+export default App;
---
+++ src/mocks/topo.js
@@ -0,0 +1,6 @@
+const topo = {
+    boasVindas: "Olá Kaique",
+    legenda: "Encontre os melhores produtores",
+};
+
+export default topo;
