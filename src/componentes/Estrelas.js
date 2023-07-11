ea3cb230a52682a0d42d88f540c791262c5a1a51
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2023-07-10 22:32:35 -0300

    :dart: Refactor estrelas

--- src/componentes/Estrelas.js
+++ src/componentes/Estrelas.js
@@ -1,52 +1,37 @@
 import React, { useState } from "react";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
-import estrela from '../assets/estrela.png';
-import estrelaCinza from '../assets/estrelaCinza.png';
+import { StyleSheet, View } from "react-native";
+import Estrela from "./Estrela";
 
-export default function Estrelas({ 
+export default function Estrelas({
     quantidade: quantidadeAntiga,
     editavel = false,
     grande = false,
-}){
+}) {
     const [ quantidade, setQuantidade ] = useState(quantidadeAntiga);
-    const estilos = estilosFuncao(grande);
 
-    const getImage = (index) => {
-        if(index < quantidade){
-            return estrela;
-        }
-        return estrelaCinza;
-    }
-    
     const RenderEstrelas = () => {
         const listaEstrelas = [];
-        for (let i=0; i<5; i++){
+        for (let i = 0; i < 5; i++) {
             listaEstrelas.push(
-                <TouchableOpacity 
+                <Estrela
                     key={i}
-                    onPress={()=> setQuantidade(i + 1)}
-                    disabled={!editavel}
-                >
-                    <Image source={getImage(i)} style={estilos.estrela}/>
-                </TouchableOpacity>
-                
+                    onPress={() => setQuantidade(i + 1)}
+                    desabilitada={!editavel}
+                    preenchida={i < quantidade}
+                    grande={grande} />
             );
         }
+
         return listaEstrelas;
     }
 
     return <View style={estilos.estrelas}>
-        <RenderEstrelas/>
+        <RenderEstrelas />
     </View>
 }
 
-const estilosFuncao = (grande)=> StyleSheet.create({
+const estilos = StyleSheet.create({
     estrelas: {
         flexDirection: 'row',
     },
-    estrela: {
-        width: grande ? 36 : 12,
-        height: grande ? 36 : 12,
-        marginRight: 2
-    }
-})
+});
